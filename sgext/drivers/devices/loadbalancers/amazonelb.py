0941763e390522ceb095b9166cb67444b27ec946
Paul Lathrop <paul@simplegeo.com>
2011-09-06 11:40:06 -0700

    PEP8/pylint!

--- sgext/drivers/devices/loadbalancers/amazonelb.py
+++ sgext/drivers/devices/loadbalancers/amazonelb.py
@@ -15,10 +15,13 @@ from sgext.util.aws import get_credentials
 
 
 class SGELBException(SGException):
+    """Exception raised by AmazonELB instances when errors occur."""
     pass
 
 
 class AmazonELB(BasicAppliance):
+    """Driver for modeling and controlling Amazon Elastic Load
+    Balancers."""
     _driver_name = 'amazonelb'
 
     def __init__(self, name, elbname, **kwargs):
@@ -26,37 +29,43 @@ class AmazonELB(BasicAppliance):
         self.set_attr(key='elb', subkey='name', value=elbname)
         self.credentials = get_credentials()
 
-    def get_boto_connection(self):
-        region = self.attr_value(key='ec2', subkey='region', merge_container_attrs=True)
+    def _get_boto_connection(self):
+        """Internal method. Returns the boto connection object for this ELB."""
+        region = self.attr_value(key='ec2', subkey='region',
+                                 merge_container_attrs=True)
         if region is None:
             raise SGELBException('Cannot find attribute with key="ec2", '
                                  'subkey="region" on AmazonELB object named '
                                  '"%s" or any of it\'s parents.' % self.name)
         conn = boto.ec2.elb.connect_to_region(region)
         if conn is None:
-            raise SGELBException('Could not establish connection to region %s' % region)
+            raise SGELBException('Could not establish connection to region %s'
+                                 % region)
         return conn
 
     def enable_zone(self, name_or_entity):
-        conn = self.get_boto_connection()
+        conn = self._get_boto_connection()
 
         if isinstance(name_or_entity, str):
             name = name_or_entity
         else:
             name = name_or_entity.name
 
-        conn.enable_availability_zones(self.attr_value(key='elb', subkey='name'), [name])
+            conn.enable_availability_zones(self.attr_value(key='elb', subkey='name'),
+                                       [name])
 
     def disable_zone(self, name_or_entity):
-        conn = self.get_boto_connection()
+        conn = self._get_boto_connection()
 
         if isinstance(name_or_entity, str):
             name = name_or_entity
         else:
             name = name_or_entity.name
 
-        conn.disable_availability_zones(self.attr_value(key='elb', subkey='name'), [name])
+        conn.disable_availability_zones(self.attr_value(key='elb', subkey='name'),
+                                        [name])
 
     def get_state(self):
-        conn = self.get_boto_connection()
-        return conn.describe_instance_health(self.attr_value(key='elb', subkey='name'))
+        conn = self._get_boto_connection()
+        return conn.describe_instance_health(self.attr_value(key='elb',
+                                                             subkey='name'))
