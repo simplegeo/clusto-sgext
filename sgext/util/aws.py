207bb83a7a41a36dbe27cb4b75f93fe0ae3a5625
Paul Lathrop <paul@simplegeo.com>
2011-09-06 13:16:24 -0700

    Fix get_credentials so it returns a dict.

--- sgext/util/aws.py
+++ sgext/util/aws.py
@@ -12,16 +12,17 @@ import boto.pyami.config as boto_config
 
 
 def get_credentials(batch=False):
-    """Return a tuple (key, secret) of AWS credentials. Credentials
-    are loaded via boto first (which checks the environment and a
-    couple well-known files). If boto cannot find any credentials, and
-    the 'batch' kwarg is set to False, this method will request
+    """Return a dictionary of AWS credentials. Credentials are loaded
+    via boto first (which checks the environment and a couple
+    well-known files). If boto cannot find any credentials, and the
+    'batch' kwarg is set to False, this method will request
     credentials from the user interactively via the console."""
     config = boto_config.Config()
     key = config.get('Credentials', 'aws_access_key_id', False)
     secret = config.get('Credentials', 'aws_secret_access_key', False)
     if key and secret:
-        return (key, secret)
+        return {'aws_access_key_id': key,
+                'aws_secret_access_key': secret}
     if batch:
         return None
     return prompt_for_credentials()
@@ -29,10 +30,11 @@ def get_credentials(batch=False):
 
 def prompt_for_credentials():
     """Prompt the user to enter their AWS credentials, and return them
-    as a tuple of (key, secret)."""
+    as a dictionary."""
     print 'Could not load AWS credentials from environment or boto configuration.'
     print 'Please enter your AWS credentials.'
     print
     key = raw_input('AWS Access Key ID: ')
     secret = getpass('AWS Secret Access Key: ')
-    return (key, secret)
+    return {'aws_access_key_id': key,
+            'aws_secret_access_key': secret}
